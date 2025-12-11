1ec19fee2bc63a77ddce04b325ee820f6dfed42c
fraustoboy <zhasminnaa@gmail.com>
2025-12-11 15:52:16 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".outline-button").forEach(btn => {
-    btn.addEventListener("click", () => {
-      alert("Вы выбрали: " + btn.getAttribute("data-level"));
-    });
-  });
-});
